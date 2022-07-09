ea67845f06f07ce3e4ded6f9833e622c937a7120
Iyanu Sam <iyanuxn@gmail.com>
2022-07-09 22:51:08 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const slider = document.querySelector('.slider')
 Color = "#7291ff"
 slider.addEventListener("input", function () {
     slider.style.background = `linear-gradient(to right, ${Color} ${this.value}%, #e0e0e0 ${this.value}%)`
-    range.textContent = this.value
     if (this.value > 30 && this.value < 50 ) {Color = "#2d4fc9"}
     if (this.value > 50 && this.value < 70 ) {Color = "#133097"}
     if (this.value > 70)  {Color = "#00155e"}
